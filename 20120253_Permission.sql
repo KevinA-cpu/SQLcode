29f7cb5c696f7c51d8b050ab51905cf57390bdba
KevinA-cpu <kevinanderson1410@gmail.com>
2022-10-04 17:41:16 +0700

    #second sql

---
+++ 20120253_Permission.sql
@@ -0,0 +1,7 @@
+EXEC sp_addlogin 'GV01', 'GV01', 'GiaoVien'
+EXEC sp_addlogin 'GV02', 'GV02', 'GiaoVien'
+EXEC sp_addlogin 'GV03', 'GV03', 'GiaoVien'
+
+EXEC sp_addlogin 'SV01', 'SV01', 'SinhVien'
+EXEC sp_addlogin 'SV02', 'SV02', 'SinhVien'
+EXEC sp_addlogin 'SV03', 'SV03', 'SinhVien'
